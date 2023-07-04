6cf458fb526cb3191dbaf55e84ecdcbd51bd08c0
Yura Korol <129438074+YuraTheKing@users.noreply.github.com>
2023-07-04 23:47:09 +0300

    Some improvements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox({alwaysShowClose: true});
-});
+// $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+//     event.preventDefault();
+//     $(this).ekkoLightbox({alwaysShowClose: true});
+// });
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
